392968a49a01dac10e63fc06fa21cec9a3fd911f
HassanAlahmed <joker.barca@gmail.com>
2017-11-05 22:12:11 -0700

    Adding more data

--- Tables.sql
+++ Tables.sql
@@ -9,7 +9,10 @@ primary key (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
 insert into `user_info` (`username`, `first_name`, `last_name`, `password`, `phone`, `school_year`) values
 ('hassan.alahmed@colorado.edu', 'Hassan', 'Alahmed', 'aaaaaa', '7203226544','2014'),
-('duaa.alahmed@colorado.edu', 'Duaa', 'Alahmed', 'bbbbbbb', '7203283554', '2015');
+('duaa.alahmed@colorado.edu', 'Duaa', 'Alahmed', 'bbbbbbb', '7203283554', '2015'),
+('jack.spicer@colorado.edu', 'jack', 'spicer', 'cccccc', '7987789845','2015'),
+('john.phillips@colorado.edu', 'john', 'phillips', 'dddddd', '89756444442', '2015'),
+('Sousheel.vunnam@colorado.edu', 'Sousheel', 'vunnam', 'eeeeee', '7438597423', '2014');
 
 
 create table if not exists `events` (
@@ -24,7 +27,10 @@ primary key (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
 insert into `events` (`id`,`username`, `location`, `departure`, `arrival`, `description`) values
 (1,'hassan.alahmed@colorado.edu', 'Keystone', '8:00am', '4:00pm', 'Going to Keystone'),
-(2,'hassan.alahmed@colorado.edu', 'Breckenridge', '8:00am', '4:00pm', 'Going to Breckenridge');
+(2,'hassan.alahmed@colorado.edu', 'Breckenridge', '7:00am', '3:00pm', 'Going to Breckenridge'),
+(3,'jack.spicer@colorado.edu', 'Copper Mt', '8:30am', '5:00pm', 'Going to Copper Mt'),
+(4,'john.phillips@colorado.edu', 'Winter Park', '9:00am', '6:00pm', 'Going to Winter Park'),
+(5,'Sousheel.vunnam@colorado.edu', 'Vail', '6:00am', '2:00pm', 'Going to Vail');
 
 create table if not exists `driver_info` (
 `username` varchar(40) not null,
@@ -35,5 +41,6 @@ primary key (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
 insert into `driver_info` (`username`, `car_model`, `car_year`, `seats`) values
 ('hassan.alahmed@colorado.edu', 'Toyota', '2008', '4'),
-('duaa.alahmed@colorado.edu', 'VW', '2008', '3');
+('duaa.alahmed@colorado.edu', 'VW', '2010', '3'),
+('jack.spicer@colorado.edu', 'Nissan', '2015', '5');
 
