a627a526542ddee43e0b104540b49345f3fa588d
Palmphillips <30354827+Palmphillips@users.noreply.github.com>
2017-11-05 20:26:49 -0700

    Updated

--- SkiUv0.1/tables.sql
+++ SkiUv0.1/tables.sql
@@ -1,25 +1,38 @@
 create table if not exists `user_info` (
 `username` varchar(40) not null,
-`name` varchar(40) not null,
 `password` varchar(40) not null,
+`first_name` varchar(40) not null,
+`last_name` varchar(40) not null,
 `phone` varchar(10) not null,
+`school_year` varchar(4),
 primary key (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
-insert into `user_info` (`username`, `name`, `password`, `phone`) values
-('hassan.alahmed@colorado.edu', 'Hassan Alahmed', 'aaaaaa', '7203226544'),
-('duaa.alahmed@colorado.edu', 'Duaa Alahmed', 'bbbbbbb', '7203283554');
+insert into `user_info` (`username`, `first_name`, `last_name`, `password`, `phone`, `school_year`) values
+('hassan.alahmed@colorado.edu', 'Hassan', 'Alahmed', 'aaaaaa', '7203226544','2014'),
+('duaa.alahmed@colorado.edu', 'Duaa', 'Alahmed', 'bbbbbbb', '7203283554', '2015');
 
 
-create table if not exists `event` (
+create table if not exists `events` (
 `id` int(1) not null auto_increment,
 `username` varchar(40) not null,
-`description` varchar(1000),
 `location` varchar(10) not null,
 `departure` varchar(10) not null,
 `arrival` varchar(10) not null,
-`passengers` varchar(100),
+`description` varchar(1000),
+`passangers` varchar(100),
 primary key (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
-insert into `event` (`id`,`username`, `description`, `location`, `departure`, `arrival`) values
-(1,'hassan.alahmed@colorado.edu', 'Going to Keystone', 'Keystone', '8:00am', '4:00pm'),
-(2,'hassan.alahmed@colorado.edu', 'Going to Breckenridge', 'Breckenridge', '8:00am', '4:00pm');
+insert into `events` (`id`,`username`, `location`, `departure`, `arrival`, `description`) values
+(1,'hassan.alahmed@colorado.edu', 'Keystone', '8:00am', '4:00pm', 'Going to Keystone'),
+(2,'hassan.alahmed@colorado.edu', 'Breckenridge', '8:00am', '4:00pm', 'Going to Breckenridge');
+
+create table if not exists `driver_info` (
+`username` varchar(40) not null,
+`car_model` varchar(20) not null,
+`car_year` varchar(4) not null,
+`seats` varchar(10),
+primary key (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
+insert into `driver_info` (`username`, `car_model`, `car_year`, `seats`) values
+('hassan.alahmed@colorado.edu', 'Toyota', '2008', '4'),
+('duaa.alahmed@colorado.edu', 'VW', '2008', '3');
