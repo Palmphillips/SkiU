beb04cb2c5a90c733b18658f6dc566099456b0ee
John <joph0064@colorado.edu>
2017-11-12 16:31:07 -0700

    Sign In Using Express

--- SkiUv0.1/Scripts.js
+++ SkiUv0.1/Scripts.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -10,6 +11,7 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
 
 connection.query('SELECT * FROM user_info', (err,rows) => {
   if(err) throw err;
@@ -22,4 +24,27 @@ rows.forEach( (row) => {
   console.log(`${row.name} is in ${row.location}`); 
 });
 
+});
+
+// Express Forms
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app     = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//app.use(express.bodyParser());
+
+
+// This takes the signin function form on SkiU.html modal login
+// This information should be used to handle login
+app.post('/signin', function(req, res) {
+  res.send('Your Email "' + req.body.email + '"' + '\n' + 'Your Password "' + req.body.pwd + '"');
+});
+
+
+
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
 });
