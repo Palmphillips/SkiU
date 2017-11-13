531ffc8547bff492684dfa89e6b066b8b569011c
sousheel <sousheel.vunnam@colorado.edu>
2017-11-13 00:09:41 -0700

    auth fixed

--- SkiUv0.2/routes/login.js
+++ SkiUv0.2/routes/login.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-app.post('/', function(req, res) {
+router.post('/', function(req, res) {
   res.send('Your Email "' + req.body.email + '"' + '\n' + 'Your Password "' + req.body.pwd + '"');
 });
 
--- SkiUv0.2/routes/register.js
+++ SkiUv0.2/routes/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-app.post('/', function(req, res) {
+router.post('/', function(req, res) {
 	connection.query("INSERT INTO user_info VALUES ('" + req.body.email + "', 'first', 'last', 'pass', '7', '2011')",
 		function (err, result) {
 			if (err) throw err;
