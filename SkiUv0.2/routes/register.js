2fcbe9b9dd59828d7a88dd1c27696b31be556c0b
sousheel <sousheel.vunnam@colorado.edu>
2017-12-01 15:51:41 -0700

    regEx registration

--- SkiUv0.2/routes/register.js
+++ SkiUv0.2/routes/register.js
@@ -27,6 +27,34 @@ router.post('/', function(req, res) {
   //
   //
   //connection.connect();
+
+  // Tests inputs using Regular Expressions
+  var emailRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!emailRe.test(req.body.email)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+  var passwordRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!passwordRe.test(req.body.password)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+  var fnameRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!fnameRe.test(req.body.fname)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+  var lnameRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!lnameRe.test(req.body.lname)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+  var phoneRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!phoneRe.test(req.body.phone)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+  var yearRe = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+  if(!yearRe.test(req.body.year)){
+    window.alert('Please enter in a valid colorado.edu email address.');
+  }
+
+
 	connection.query("INSERT INTO user_info VALUES ('" + req.body.email + "', '" + req.body.password + "', '" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.phone + "', '" + req.body.year + "')", function (err, result) {
 		if (err) throw err;
 		res.redirect('home');
