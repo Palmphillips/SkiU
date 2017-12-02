f384bd3dd78af91fbfeaa0ac8fce6e8d7f5f6852
John <joph0064@colorado.edu>
2017-12-02 14:38:34 -0700

    test

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var mysql = require('mysql');
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: 'ba8c6efcf34d52',
   password: '23eda3ad',
