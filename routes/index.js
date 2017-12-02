015d4fb64cae65b05e8821014fc3c9b8a628ddb3
jackspicer1229 <jack.spicer@colorado.edu>
2017-12-02 15:01:56 -0700

    login test

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-// var mysql = require('mysql');
+var mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'us-cdbr-iron-east-05.cleardb.net',
-  user: 'ba8c6efcf34d52',
-  password: '23eda3ad',
-  database: 'heroku_d087506ec02ec33'
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Sou91599!!',
+  database : 'SkiU'
 });
 
 /* GET home page. */
