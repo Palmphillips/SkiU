0d05c8971cd12c7eea7bb0af52093dc53bd35295
sousheel <sousheel.vunnam@colorado.edu>
2017-12-02 12:40:04 -0700

    sessions bug fixed 1

--- SkiUv0.2/routes/profile.js
+++ SkiUv0.2/routes/profile.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-// edit so that user must go to skiu.com/prfile/USERNAME
+// edit links so that user must go to skiu.com/profile?user=blah
 
 router.get('/', function(req, res, next) {
-  res.render('profile');
+  //res.render('profile');
+  if (req.query.user == req.session.email) {
+    res.send('this profile is yours');
+  }
+  else {
+    res.send('this profile is somebody elses')
+    res.redirect('/home');
+  }
 });
 
 module.exports = router;
