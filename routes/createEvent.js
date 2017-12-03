a9ccdf064d3ae58d1af73283f855f838d77ac0e6
jackspicer1229 <jack.spicer@colorado.edu>
2017-12-02 20:25:45 -0700

    create event page and js

--- routes/createEvent.js
+++ routes/createEvent.js
@@ -14,35 +14,29 @@ router.get('/', function(req, res, next) {
   res.render('createEvent');
 });
 
-router.post('/', function(req, res) {
-  // connection.connect();
-  //
-  // connection.query('SELECT * FROM events', function (err, rows, fields) {
-  //   if (err) throw err
-  //
-  //   console.log(rows[0].username);
-  //
-  // //  res.render('index', { title: 'SkiU', rows: rows });
-  // });
-  //
-  //
-  //connection.connect();
+// router.post('/', function(req, res) {
+//   // connection.connect();
+//   //
+//   // connection.query('SELECT * FROM events', function (err, rows, fields) {
+//   //   if (err) throw err
+//   //
+//   //   console.log(rows[0].username);
+//   //
+//   // //  res.render('index', { title: 'SkiU', rows: rows });
+//   // });
+//   //
+//   //
+//   //connection.connect();
 
 
 
-	// connection.query("INSERT INTO user_info VALUES ('" + req.body.email + "', '" + req.body.password + "', '" + req.body.fname + "', '" + req.body.lname + "', '" + req.body.phone + "', '" + req.body.year + "')", function (err, result) {
-	// 	if (err) throw err;
+	connection.query("INSERT INTO events VALUES ('" + "placeholder" + "', '" + req.body.location + "', '" + req.body.departure + "', '" + req.body.date + "', '" + req.body.description + "', '" + req.body.passengers + "')", function (err, result) {
+		if (err) throw err;
 
- //    // Assigning session variables to indicate that user is logged in
- //    var sess = req.session;
- //    sess.loggedIn = true;
- //    sess.email=req.body.email;
- //    sess.first_name=req.body.fname;
- //    sess.last_name=req.body.lname;
 
-	// 	res.redirect('home');
-	// });
-  //connection.end();
+		res.redirect('home');
+	});
+  connection.end();
 });
 
 module.exports = router;
